53071789b872f2ac25668c4e40d5c3a56b2d2fe5
Gurankio <gurankio@icloud.com>
2020-01-01 19:59:49 +0100

    Completed VTS

--- Libraries/vts.c
+++ Libraries/vts.c
@@ -1,6 +1,7 @@
 /*
  * VTS - Virtual Terminal Sequences
- * Partial (Full?) implementation of https://docs.microsoft.com/en-us/windows/console/console-virtual-terminal-sequences.
+ * Full implementation of https://docs.microsoft.com/en-us/windows/console/console-virtual-terminal-sequences.
+ * Some CSI / OCS are still missing.
  */
 
 #ifndef VTS
@@ -15,19 +16,31 @@
 #include <windows.h>
 #endif
 
+// // Macro
+#define vts_xy(x, y) vts_cursorXY(x, y);
+
 // // Prototypes
 
 void vts_activateCommands();
 
 // Miscellanous
-void vts_title(char *);
 void vts_resize(int, int);
 void vts_clear();
-void vts_lineDrawingSet();
-void vts_asciiSet();
 
-// Cursor
-void vts_xy(int, int);
+// Cursor Positioning
+void vts_cursorUp(int);
+void vts_cursorDown(int);
+void vts_cursorRight(int);
+void vts_cursorLeft(int);
+void vts_cursorNextLine(int);
+void vts_cursorPreviousLine(int);
+void vts_cursorHorizontalAbsolute(int);
+void vts_cursorVerticalAbsolute(int);
+void vts_cursorXY(int, int);
+void vts_cursorSave();
+void vts_cursorRestore();
+
+// Cursor Visibility
 void vts_cursorBlink();
 void vts_cursorStill();
 void vts_cursorShow();
@@ -37,42 +50,49 @@ void vts_cursorHide();
 void vts_viewportUp(int);
 void vts_viewportDown(int);
 
-// Text Modifiers
+// Text Modification
 void vts_textInsertChar(int);
 void vts_textDeleteChar(int);
 void vts_textEraseChar(int);
 void vts_textInsertLine(int);
 void vts_textDeleteLine(int);
 
-void vts_textBold();
-void vts_textLowIntesity();
+// Text Formatting
+void vts_textDefault();
+void vts_textBold(); // makes bright foreground
 void vts_textUnderline();
-void vts_textBlinking();
-void vts_textReverse();
-void vts_textInvisible();
+void vts_textNoUnderline();
+void vts_textNegative();
+void vts_textPositive();
+// TODO: Test compat in windows
+// void vts_textLowIntesity();
+// void vts_textBlinking();
+// void vts_textInvisible();
 
-void vts_textModifierReset();
+// Foreground Colors
+void vts_foregroundBlack();
+void vts_foregroundRed();
+void vts_foregroundGreen();
+void vts_foregroundYellow();
+void vts_foregroundBlue();
+void vts_foregroundMagenta();
+void vts_foregroundCyan();
+void vts_foregroundWhite();
 
-// Text Colors
-void vts_textBlack();
-void vts_textRed();
-void vts_textGreen();
-void vts_textYellow();
-void vts_textBlue();
-void vts_textMagenta();
-void vts_textCyan();
-void vts_textWhite();
+// Bright Foreground Colors
+// Can be set by "vts_textBold"
+void vts_foregroundBrightBlack();
+void vts_foregroundBrightRed();
+void vts_foregroundBrightGreen();
+void vts_foregroundBrightYellow();
+void vts_foregroundBrightBlue();
+void vts_foregroundBrightMagenta();
+void vts_foregroundBrightCyan();
+void vts_foregroundBrightWhite();
 
-void vts_textBrightBlack();
-void vts_textBrightRed();
-void vts_textBrightGreen();
-void vts_textBrightYellow();
-void vts_textBrightBlue();
-void vts_textBrightMagenta();
-void vts_textBrightCyan();
-void vts_textBrightWhite();
-
-void vts_textColorReset();
+// Advanced Foreground Colors
+void vts_foregroundExtended(int, int, int);
+void vts_foregroundDefault();
 
 // Background Colors
 void vts_backgroundBlack();
@@ -84,6 +104,7 @@ void vts_backgroundMagenta();
 void vts_backgroundCyan();
 void vts_backgroundWhite();
 
+// Bright Background Colors
 void vts_backgroundBrightBlack();
 void vts_backgroundBrightRed();
 void vts_backgroundBrightGreen();
@@ -93,7 +114,42 @@ void vts_backgroundBrightMagenta();
 void vts_backgroundBrightCyan();
 void vts_backgroundBrightWhite();
 
-void vts_backgroundReset();
+// Advanced Background Colors
+void vts_backgroundExtended(int, int, int);
+void vts_backgroundDefault();
+
+// Mode Changes
+void vts_keypadApplicationMode();
+void vts_keypadNumericMode();
+void vts_cursorKeysApplicationMode();
+void vts_cursorKeysNumericMode();
+
+// Query States?
+
+// Tabs
+void vts_tabStop();
+void vts_cursorForwardTab(int);
+void vts_cursorBackwardTab(int);
+void vts_tabClearCurrent();
+void vts_tabClearAll();
+
+// Character Set
+void vts_lineDrawingSet();
+void vts_asciiSet();
+
+// Scrolling Margin
+void vts_scrollingMargin(int, int);
+
+// Window Title
+void vts_iconTitle(char *);
+void vts_title(char *);
+
+// Alternate Screen Buffer
+void vts_alternateScreenBuffer();
+void vts_mainScreenBuffer();
+
+// Soft Reset
+void vts_softReset();
 
 // // Functions
 
@@ -133,13 +189,6 @@ void vts_activateCommands() {
 #endif /* ifdef _WIN32 */
 
 // Miscellanous
-
-void vts_title(char *title) {
-  //]0);hello\x07 unknown meanings!
-  printf("\e]0);%s\x07", title);
-  fflush(stdout);
-}
-
 void vts_resize(int width, int height) {
   // [8);50);100t what does the 8 mean?
   printf("\e[8);%d);%dt", height, width);
@@ -151,23 +200,63 @@ void vts_clear() {
   fflush(stdout);
 }
 
-void vts_lineDrawingSet() {
-  printf("\e(0");
+// Cursor Positioning
+void vts_cursorUp(int n) {
+  printf("\e[%dA", n);
   fflush(stdout);
 }
 
-void vts_asciiSet() {
-  printf("\e(B");
+void vts_cursorDown(int n) {
+  printf("\e[%dB", n);
   fflush(stdout);
 }
 
-// Cursor
+void vts_cursorRight(int n) {
+  printf("\e[%dC", n);
+  fflush(stdout);
+}
 
-void vts_xy(int x, int y) {
+void vts_cursorLeft(int n) {
+  printf("\e[%dD", n);
+  fflush(stdout);
+}
+
+void vts_cursorNextLine(int n) {
+  printf("\e[%dE", n);
+  fflush(stdout);
+}
+
+void vts_cursorPreviousLine(int n) {
+  printf("\e[%dF", n);
+  fflush(stdout);
+}
+
+void vts_cursorHorizontalAbsolute(int n) {
+  printf("\e[%dG", n);
+  fflush(stdout);
+}
+
+void vts_cursorVerticalAbsolute(int n) {
+  printf("\e[%dd", n);
+  fflush(stdout);
+}
+
+void vts_cursorXY(int x, int y) {
   printf("\e[%d);%dH", y + 1, x);
   fflush(stdout);
 }
 
+void vts_cursorSave() {
+  printf("\e[s");
+  fflush(stdout);
+}
+
+void vts_cursorRestore() {
+  printf("\e[u");
+  fflush(stdout);
+}
+
+// Cursor Visibility
 void vts_cursorBlink() {
   printf("\e[?12h");
   fflush(stdout);
@@ -189,7 +278,6 @@ void vts_cursorHide() {
 }
 
 // Viewport
-
 void vts_viewportUp(int n) {
   printf("\e[%dS", n);
   fflush(stdout);
@@ -200,8 +288,7 @@ void vts_viewportDown(int n) {
   fflush(stdout);
 }
 
-// Text Modifier
-
+// Text Modification
 void vts_textInsertChar(int n) {
   printf("\e[%d@", n);
   fflush(stdout);
@@ -227,13 +314,14 @@ void vts_textDeleteLine(int n) {
   fflush(stdout);
 }
 
-void vts_textBold() {
+// Text Formatting
+void vts_textDefault() {
   printf("\e[1m");
   fflush(stdout);
 }
 
-void vts_textLowIntesity() {
-  printf("\e[2m");
+void vts_textBold() {
+  printf("\e[1m");
   fflush(stdout);
 }
 
@@ -242,119 +330,115 @@ void vts_textUnderline() {
   fflush(stdout);
 }
 
-void vts_textBlinking() {
-  printf("\e[5m");
+void vts_textNoUnderline() {
+  printf("\e[24m");
   fflush(stdout);
 }
 
-void vts_textReverse() {
+void vts_textNegative() {
   printf("\e[7m");
   fflush(stdout);
 }
 
-void vts_textInvisible() {
-  printf("\e[8m");
+void vts_textPositive() {
+  printf("\e[27m");
   fflush(stdout);
 }
 
-void vts_textModifierReset() {
-  printf("\e[9m");
-  fflush(stdout);
-}
-
-// Text Colors
-
-void vts_textBlack() {
+// Foreground Colors
+void vts_foregroundBlack() {
   printf("\e[30m");
   fflush(stdout);
 }
 
-void vts_textRed() {
+void vts_foregroundRed() {
   printf("\e[31m");
   fflush(stdout);
 }
 
-void vts_textGreen() {
+void vts_foregroundGreen() {
   printf("\e[32m");
   fflush(stdout);
 }
 
-void vts_textYellow() {
+void vts_foregroundYellow() {
   printf("\e[33m");
   fflush(stdout);
 }
 
-void vts_textBlue() {
+void vts_foregroundBlue() {
   printf("\e[34m");
   fflush(stdout);
 }
 
-void vts_textMagenta() {
+void vts_foregroundMagenta() {
   printf("\e[35m");
   fflush(stdout);
 }
 
-void vts_textCyan() {
+void vts_foregroundCyan() {
   printf("\e[36m");
   fflush(stdout);
 }
 
-void vts_textWhite() {
+void vts_foregroundWhite() {
   printf("\e[37m");
   fflush(stdout);
 }
 
-//
-
-void vts_textBrightBlack() {
+// Bright Foreground Colors
+void vts_foregroundBrightBlack() {
   printf("\e[91m");
   fflush(stdout);
 }
 
-void vts_textBrightRed() {
+void vts_foregroundBrightRed() {
   printf("\e[91m");
   fflush(stdout);
 }
 
-void vts_textBrightGreen() {
+void vts_foregroundBrightGreen() {
   printf("\e[92m");
   fflush(stdout);
 }
 
-void vts_textBrightYellow() {
+void vts_foregroundBrightYellow() {
   printf("\e[93m");
   fflush(stdout);
 }
 
-void vts_textBrightBlue() {
+void vts_foregroundBrightBlue() {
   printf("\e[94m");
   fflush(stdout);
 }
 
-void vts_textBrightMagenta() {
+void vts_foregroundBrightMagenta() {
   printf("\e[95m");
   fflush(stdout);
 }
 
-void vts_textBrightCyan() {
+void vts_foregroundBrightCyan() {
   printf("\e[96m");
   fflush(stdout);
 }
 
-void vts_textBrightWhite() {
+void vts_foregroundBrightWhite() {
   printf("\e[97m");
   fflush(stdout);
 }
 
-//
+// Advanced Foreground Colors
+void vts_foregroundExtended(int r, int g, int b) {
+  printf("\e[38;2;%d;%d;%dm", r, g, b);
+  fflush(stdout);
+}
 
-void vts_textColorReset() {
+void vts_foregroundDefault() {
   printf("\e[39m");
   fflush(stdout);
 }
 
 // Background Colors
-
 void vts_backgroundBlack() {
   printf("\e[40m");
   fflush(stdout);
@@ -395,8 +479,7 @@ void vts_backgroundWhite() {
   fflush(stdout);
 }
 
-//
-
+// Bright Background Colors
 void vts_backgroundBrightBlack() {
   printf("\e[100m");
   fflush(stdout);
@@ -437,10 +520,108 @@ void vts_backgroundBrightWhite() {
   fflush(stdout);
 }
 
-//
+// Advanced Background Colors
+void vts_backgroundExtended(int r, int g, int b) {
+  printf("\e[48;2;%d;%d;%dm", r, g, b);
+  fflush(stdout);
+}
 
-void vts_backgroundReset() {
+void vts_backgroundDefault() {
   printf("\e[49m");
+  fflush(stdout);
+}
+
+// Mode Changes
+void vts_keypadApplicationMode() {
+  printf("\e=");
+  fflush(stdout);
+}
+
+void vts_keypadNumericMode() {
+  printf("\e>");
+  fflush(stdout);
+}
+
+void vts_cursorKeysApplicationMode() {
+  printf("\e[?1h");
+  fflush(stdout);
+}
+
+void vts_cursorKeysNumericMode() {
+  printf("\e[?1l");
+  fflush(stdout);
+}
+
+// Query States?
+
+// Tabs
+void vts_tabStop() {
+  printf("\eH");
+  fflush(stdout);
+}
+
+void vts_cursorForwardTab(int n) {
+  printf("\e[%dI", n);
+  fflush(stdout);
+}
+
+void vts_cursorBackwardTab(int n) {
+  printf("\e[%dZ", n);
+  fflush(stdout);
+}
+
+void vts_tabClearCurrent() {
+  printf("\e[0g");
+  fflush(stdout);
+}
+
+void vts_tabClearAll() {
+  printf("\e[3g");
+  fflush(stdout);
+}
+
+// Character Set
+void vts_lineDrawingSet() {
+  printf("\e(0");
+  fflush(stdout);
+}
+
+void vts_asciiSet() {
+  printf("\e(B");
+  fflush(stdout);
+}
+
+// Scrolling Margins
+void vts_scrollingMargin(int t, int b) {
+  printf("\e[%d;%dr", t, b);
+  fflush(stdout);
+}
+
+// Window Title
+void vts_iconTitle(char *title) {
+  printf("\e]0);%s\x07", title);
+  fflush(stdout);
+}
+
+void vts_title(char *title) {
+  printf("\e]2);%s\x07", title);
+  fflush(stdout);
+}
+
+// Alternate Screen Buffer
+void vts_alternateScreenBuffer() {
+  printf("\e[1049h");
+  fflush(stdout);
+}
+
+void vts_mainScreenBuffer() {
+  printf("\e[1049l");
+  fflush(stdout);
+}
+
+// Soft Reset
+void vts_softReset() {
+  printf("\e[!p");
   fflush(stdout);
 }
 
