7d15dad64af938445655be17eeff5489b892f432
Gurankio <gurankio@icloud.com>
2020-01-21 14:54:37 +0100

    Desc

--- #084 Squadre.c
+++ #084 Squadre.c
@@ -1,6 +1,6 @@
 /*
  * Jacopo Del Granchio
- * #084 21.01.20
+ * #084 21.01.2020
  *
  * Lorem ipsum dolor sit amet, consectetur adipisicing elit,
  * sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
