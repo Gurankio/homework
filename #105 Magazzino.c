85b1d0eea107aa61b3b2f88867a0a11df322dabf
Gurankio <gurankio@icloud.com>
2020-03-13 10:32:18 +0100

    Sassi TODO

---
+++ #105 Magazzino.c
@@ -0,0 +1,32 @@
+/*
+ * Jacopo Del Granchio
+ * #105 ??.03.2020
+ *
+ * Articolo, Quantità, Prezzo.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+#include <stdarg.h>
+#include <stdbool.h>
+
+// Macro
+#define chiedi(msg, format, ...) \
+  printf(msg); \
+  scanf(format, __VA_ARGS__);
+
+// Prototipi
+
+
+// Funzioni
+int main() {
+  setlocale(LC_ALL, "");
+
+
+
+  // getchar();
+  // system("pause");
+  return 0;
+}
---
+++ #106 Ristorante.c
@@ -0,0 +1,32 @@
+/*
+ * Jacopo Del Granchio
+ * #106 13.03.2020
+ *
+ * Nome, Localita, Coperti, Stelle, Menu (Nome, Costo, Glutine).
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+#include <stdarg.h>
+#include <stdbool.h>
+
+// Macro
+#define chiedi(msg, format, ...) \
+  printf(msg); \
+  scanf(format, __VA_ARGS__);
+
+// Prototipi
+
+
+// Funzioni
+int main() {
+  setlocale(LC_ALL, "");
+
+
+
+  // getchar();
+  // system("pause");
+  return 0;
+}
