2b702ee55dcd11512d3d3d7020a6b4ca2c79ea6c
Gurankio <gurankio@icloud.com>
2020-11-05 11:25:27 +0100

    Sassi.

---
+++ Java/#003 Persone/src/gurankio/Main.java
@@ -0,0 +1,10 @@
+package gurankio;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Persona p = new Persona(1123, "Arcibaldo", true, "Informatico");
+        System.out.println(p.toString());
+    }
+
+}
