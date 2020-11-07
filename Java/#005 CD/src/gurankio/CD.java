1366fecdc672c09d543a79280ece906b55f4e921
Gurankio <gurankio@icloud.com>
2020-11-07 10:21:47 +0100

    Sasso

--- Java/#005 CD/src/gurankio/CD.java
+++ Java/#005 CD/src/gurankio/CD.java
@@ -1,2 +1,63 @@
-package gurankio;public class CD {
+package gurankio;
+
+public class CD {
+
+    private String titolo;
+    private String autore;
+    private int numeroBrani;
+    private int durata; // in secondi interi.
+
+    public CD(String titolo, String autore, int numeroBrani, int durata) {
+        this.titolo = titolo;
+        this.autore = autore;
+        this.numeroBrani = numeroBrani;
+        this.durata = durata;
+    }
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public String getAutore() {
+        return autore;
+    }
+
+    public void setAutore(String autore) {
+        this.autore = autore;
+    }
+
+    public int getNumeroBrani() {
+        return numeroBrani;
+    }
+
+    public void setNumeroBrani(int numeroBrani) {
+        this.numeroBrani = numeroBrani;
+    }
+
+    public int getDurata() {
+        return durata;
+    }
+
+    public void setDurata(int durata) {
+        this.durata = durata;
+    }
+
+    public int compareDurata(CD cd) {
+        if (getDurata() == cd.getDurata()) return 0;
+        return getDurata() > cd.getDurata() ? 1 : -1;
+    }
+
+    @Override
+    public String toString() {
+        return "CD{" +
+                "titolo='" + titolo + '\'' +
+                ", autore='" + autore + '\'' +
+                ", numeroBrani=" + numeroBrani +
+                ", durata=" + durata +
+                '}';
+    }
 }
--- Java/#005 CD/src/gurankio/Main.java
+++ Java/#005 CD/src/gurankio/Main.java
@@ -3,7 +3,13 @@ package gurankio;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
+		CD cd1 = new CD("Hello,", "World!", 6, 1200);
+		System.out.println(cd1);
+
+		CD cd2 = new CD("AAAA", "BBBB", 612013, 1);
+		System.out.println(cd2);
+
+		System.out.println("Compare: " + cd1.compareDurata(cd2));
 	}
 
 }
