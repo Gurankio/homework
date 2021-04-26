7a8593e878ffd6287bea8eb201f8f9e61b278d58
Gurankio <gurankio@icloud.com>
2021-04-26 10:43:17 +0200

    sasso

--- C/#112 Matematica.c
+++ C/#112 Matematica.c
@@ -12,30 +12,32 @@
 
 #define malloc_shared(size) mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
 
-enum { a, b, c, d, e };
+enum { a, b, c, d, e, f };
 
 int main(int argc, char const *argv[]) {
-    int* s = malloc_shared(sizeof(int) * 5);
+    int* s = malloc_shared(sizeof(int) * 6);
+    
+    // (7 * 2) + (25 : 5) * (8 - 5) 
 
     if (!fork()) {
         // child
         if (!fork()) {
             // child
-            s[c] = 18 / 9;
+            s[c] = 8 - 5;
             exit(0);
         } else {
             // father
-            s[b] = 6 - 4;
+            s[b] = 25 / 5;
             wait(NULL);
             s[d] = s[b] * s[c]; 
             exit(0);
         }
     } else {
         // father
-        s[a] = 3 + 2;
+        s[a] = 7 * 2;
         wait(NULL);
         s[e] = s[a] + s[d]; 
-        printf("Risultato: %d\n", 9);
+        printf("Risultato: %d\n", s[e]);
     }
 
     return 0;
